948213be5af0ac724f5152124015197076d8b0c0
WolFrame1489 <dlaritchev@hotmail.com>
2021-03-08 12:41:10 +0300

    init

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static System.Math;
+
+
+public class Main : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
